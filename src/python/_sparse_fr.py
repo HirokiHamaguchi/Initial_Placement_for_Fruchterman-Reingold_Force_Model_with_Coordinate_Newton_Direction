c49386f8070a9ae9db9e0e8b2d304ee4a3544be9
hari64 <hari64boli64@gmail.com>
2024-08-17 10:44:47 +0900

    save temp

--- src/python/_sparse_fr.py
+++ src/python/_sparse_fr.py
@@ -93,7 +93,7 @@ def _sparse_fruchterman_reingold(
             if (np.linalg.norm(delta_pos) / nnodes) < threshold:
                 break
             yield pos
-    elif method == "BFGS":
+    elif method == "L-BFGS-B" or method == "CG":
         # make sure we have a coo_matrix representation
         try:
             A = A.tolil()
@@ -107,6 +107,7 @@ def _sparse_fruchterman_reingold(
             grad = np.zeros((nnodes, dim))
             for i in range(nnodes):
                 delta = pos[i] - pos
+                assert np.all(delta[i] == 0.0)
                 distance = np.linalg.norm(delta, axis=1)
                 distance = np.where(distance < 0.01, 0.01, distance)
                 distance_inv = 1 / distance
@@ -121,7 +122,7 @@ def _sparse_fruchterman_reingold(
         sp.optimize.minimize(
             cost_fun,
             pos.ravel(),
-            method="L-BFGS-B",
+            method="L-BFGS-B" if method == "L-BFGS-B" else "CG",
             jac=True,
             options={"maxiter": iterations, "disp": verbose},
             callback=lambda x: pos_hist.append(x.reshape((nnodes, dim))),
@@ -130,118 +131,219 @@ def _sparse_fruchterman_reingold(
         for pos in pos_hist:
             yield pos
 
-    elif method == "RS":
+    elif method == "myCG":
+        from scipy.optimize._optimize import _line_search_wolfe12
+
         # make sure we have a coo_matrix representation
         try:
             A = A.tolil()
         except AttributeError:
             A = (sp.sparse.coo_array(A)).tolil()
 
-<<<<<<< HEAD
+        k_inv = 1 / k
+
+        def f(x: np.ndarray) -> float:
+            pos = x.reshape((nnodes, dim))
+            ret = cost(pos, A, k)
+            print(f"{ret=}")
+            return ret
+
+        def myfprime(x: np.ndarray) -> np.ndarray:
+            pos = x.reshape((nnodes, dim))
+            grad = np.zeros((nnodes, dim))
+            for i in range(nnodes):
+                delta = pos[i] - pos
+                assert np.all(delta[i] == 0.0)
+                distance = np.linalg.norm(delta, axis=1)
+                distance = np.where(distance < 0.01, 0.01, distance)
+                distance_inv = 1 / distance
+                Ai = A.getrow(i).toarray().flatten()
+                coefficient1 = Ai * distance * k_inv - (k * distance_inv) ** 2
+                grad[i] = coefficient1 @ delta
+            return grad.ravel()
+
+        x0 = pos.ravel()
+        gtol = 1e-5
+        maxiter = 200
+        disp = True
+        c1 = 1e-4
+        c2 = 0.4
+
+        old_fval = f(x0)
+        gfk = myfprime(x0)
+
+        k_cg = 0
+        xk = x0
+        old_old_fval = old_fval + np.linalg.norm(gfk) / 2
+
+        warnflag = 0
+        pk = -gfk
+        gnorm = np.amax(np.abs(gfk))
+
+        sigma_3 = 0.01
+
+        while (gnorm > gtol) and (k_cg < maxiter):
+            deltak = np.dot(gfk, gfk)
+
+            cached_step = [None]
+
+            def polak_ribiere_powell_step(alpha, gfkp1=None):
+                xkp1 = xk + alpha * pk
+                if gfkp1 is None:
+                    gfkp1 = myfprime(xkp1)
+                yk = gfkp1 - gfk
+                beta_k = max(0, np.dot(yk, gfkp1) / deltak)
+                pkp1 = -gfkp1 + beta_k * pk
+                gnorm = np.amax(np.abs(gfkp1))
+                return (alpha, xkp1, pkp1, gfkp1, gnorm)
+
+            def descent_condition(alpha, xkp1, fp1, gfkp1):
+                # Polak-Ribiere+ needs an explicit check of a sufficient
+                # descent condition, which is not guaranteed by strong Wolfe.
+                #
+                # See Gilbert & Nocedal, "Global convergence properties of
+                # conjugate gradient methods for optimization",
+                # SIAM J. Optimization 2, 21 (1992).
+                cached_step[:] = polak_ribiere_powell_step(alpha, gfkp1)
+                alpha, _, pk, gfk, gnorm = cached_step
+
+                # Accept step if it leads to convergence.
+                if gnorm <= gtol:
+                    return True
+
+                # Accept step if sufficient descent condition applies.
+                return np.dot(pk, gfk) <= -sigma_3 * np.dot(gfk, gfk)
+
+            try:
+                alpha_k, _, _, old_fval, old_old_fval, gfkp1 = _line_search_wolfe12(
+                    f,
+                    myfprime,
+                    xk,
+                    pk,
+                    gfk,
+                    old_fval,
+                    old_old_fval,
+                    c1=c1,
+                    c2=c2,
+                    amin=1e-100,
+                    amax=1e100,
+                    extra_condition=descent_condition,
+                )
+            except RuntimeError:
+                # Line search failed to find a better solution.
+                warnflag = 2
+                break
+
+            # Reuse already computed results if possible
+            if alpha_k == cached_step[0]:
+                alpha_k, xk, pk, gfk, gnorm = cached_step
+            else:
+                alpha_k, xk, pk, gfk, gnorm = polak_ribiere_powell_step(alpha_k, gfkp1)
+
+            k_cg += 1
+
+            yield xk.reshape((nnodes, dim))
+
+        fval = old_fval
+        if disp:
+            if warnflag == 2:
+                msg = "pr_loss"
+            elif k_cg >= maxiter:
+                warnflag = 1
+                msg = "maxiter"
+            elif np.isnan(gnorm) or np.isnan(fval) or np.isnan(xk).any():
+                warnflag = 3
+                msg = "nan"
+            else:
+                msg = "success"
+            print("         WarnFlag and message: %d" % warnflag, msg)
+            print("         Current function value: %f" % fval)
+            print("         Iterations: %d" % k)
+
+    elif method == "SCG":
+        # make sure we have a coo_matrix representation
+        try:
+            A = A.tolil()
+        except AttributeError:
+            A = (sp.sparse.coo_array(A)).tolil()
+
         vertices = np.delete(np.arange(nnodes), fixed)
         k_inv = 1 / k
 
         for it in range(iterations):
             # t = (it / iterations) ** 0.5
             t = 1
-=======
-        k_inv = 1 / k
-
-        vertices = np.delete(np.arange(nnodes), fixed)
-        for iteration in range(iterations):
-            t = (iteration / iterations) ** 0.5
-            # t = 1
->>>>>>> d9f1ca8d5ce63e88bf4575a054212c45f993ae60
             np.random.shuffle(vertices)
 
             for i in vertices:
 
-<<<<<<< HEAD
                 def cost_fun_i(x):
-                    # todo iの除外
                     assert t == 1, "change np.log for t!=1"
                     delta = x - pos
+                    delta[i] = 0.0
                     distance = np.linalg.norm(delta, axis=1)
                     distance = np.where(distance < 0.01, 0.01, distance)
                     distance_inv = 1 / distance
                     Ai = A.getrow(i).toarray().flatten()
-                    coefficient1 = Ai * distance * k_inv - (k * distance_inv) ** 2
-                    coefficient2 = Ai * distance_inv * k_inv + ((1 + t) * k**2) * (
-                        distance_inv ** (3 + t)
-                    )
                     cost = np.sum(
                         Ai * distance**3 * (1 / 3) * k_inv - (k**2) * np.log(distance)
-=======
-                hess_memo = dict()
-
-                def cost_fun(x):
-                    grad = np.zeros(dim)
-                    hess = np.zeros((dim, dim))
-                    delta = x - pos
-                    delta[i] = 0
-                    distance = np.linalg.norm(delta, axis=1)
-                    distance = np.where(distance < 0.001, 0.001, distance)
-                    distance_inv = 1 / distance
-                    Ai = A.getrow(i).toarray().flatten()
-                    coefficient1 = Ai * distance * k_inv - (k**2) * (
-                        distance_inv ** (1 + t)
                     )
-                    coefficient2 = Ai * distance_inv * k_inv + ((1 + t) * k**2) * (
-                        distance_inv ** (3 + t)
-                    )
-                    coefficient1[i] = 0
-                    coefficient2[i] = 0
-                    cost = np.sum(
-                        Ai * (distance**3) * (1 / 3) * k_inv
-                        - (k**2)
-                        * (
-                            np.log(distance)
-                            if t == 1
-                            else (distance ** (1 - t) - 1) / (1 - t)
-                        )
->>>>>>> d9f1ca8d5ce63e88bf4575a054212c45f993ae60
-                    )
-                    grad = coefficient1 @ delta
-                    hess = np.sum(coefficient1) * np.eye(dim) + np.einsum(
-                        "ij,ik->jk", coefficient2[:, np.newaxis] * delta, delta
-                    )
-<<<<<<< HEAD
+                    grad = (Ai * distance * k_inv - (k * distance_inv) ** 2) @ delta
 
-                    return cost, grad, hess
-
-                pos_hist = []
-                sp.optimize.minimize(
-                    cost_fun_i,
-                    pos.ravel(),
-                    method="L-BFGS-B",
-                    jac=True,
-                    options={"maxiter": iterations, "disp": verbose},
-                    callback=lambda x: pos_hist.append(x.reshape((nnodes, dim))),
-                )
-
-                pos[i] += delta_pos
-=======
-                    hess_memo[tuple(x.tolist())] = hess
                     return cost, grad
 
-                def hess_fun(x):
-                    key = tuple(x.tolist())
-                    assert key in hess_memo
-                    return hess_memo[key]
-
-                result = sp.optimize.minimize(
-                    cost_fun,
+                pos[i] = sp.optimize.minimize(
+                    cost_fun_i,
                     pos[i],
-                    method="Newton-CG",
                     jac=True,
-                    hess=hess_fun,
                     options={"maxiter": 3},
-                )
-                # assert result.success
-                pos[i] = result.x
+                ).x
 
->>>>>>> d9f1ca8d5ce63e88bf4575a054212c45f993ae60
             yield pos
+
+            if verbose:
+                print(f"{cost(pos,A,k)=}")
+    elif method == "SCCG":
+        # make sure we have a coo_matrix representation
+        try:
+            A = A.tolil()
+        except AttributeError:
+            A = (sp.sparse.coo_array(A)).tolil()
+
+        vertices = np.delete(np.arange(nnodes), fixed)
+        k_inv = 1 / k
+
+        for it in range(iterations):
+            # t = (it / iterations) ** 0.5
+            t = 1
+            np.random.shuffle(vertices)
+
+            for i in vertices:
+
+                def cost_fun_i(x):
+                    assert t == 1, "change np.log for t!=1"
+                    delta = x - pos
+                    delta[i] = 0.0
+                    distance = np.linalg.norm(delta, axis=1)
+                    distance = np.where(distance < 0.01, 0.01, distance)
+                    distance_inv = 1 / distance
+                    Ai = A.getrow(i).toarray().flatten()
+                    cost = np.sum(
+                        Ai * distance**3 * (1 / 3) * k_inv - (k**2) * np.log(distance)
+                    )
+                    grad = (Ai * distance * k_inv - (k * distance_inv) ** 2) @ delta
+
+                    return cost, grad
+
+                pos[i] = sp.optimize.minimize(
+                    cost_fun_i,
+                    pos[i],
+                    jac=True,
+                    options={"maxiter": 3},
+                ).x
+
+            yield pos
+
             if verbose:
                 print(f"{cost(pos,A,k)=}")
     else:
@@ -256,5 +358,5 @@ if __name__ == "__main__":
     mat = np.array([[0, 1, 2], [1, 0, -1], [2, -1, 0]])
     mat = scipy.sparse.csr_matrix(mat)
     G = nx.Graph(mat)
-    pos = spring_layout(G, iterations=50, seed=0, method="RS")
+    pos = spring_layout(G, iterations=50, seed=0, method="BFGS")
     print(pos)
