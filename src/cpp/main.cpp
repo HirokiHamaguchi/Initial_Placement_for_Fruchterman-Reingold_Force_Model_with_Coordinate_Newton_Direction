c65b1f85671423d4a98d1c9b1fdf172add4b9549
hari64 <hari64boli64@gmail.com>
2024-09-10 11:51:55 +0900

    update c++

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,42 +1,139 @@
+#include <algorithm>
+#include <chrono>
 #include <cmath>
 #include <iostream>
+#include <numeric>
+#include <random>
+#include <set>
 #include <vector>
 
-std::pair<float, float> axialCoord2Euclidean(int q, int r) {
-  return {q + r / 2.0, r * std::sqrt(3) / 2.0};
+#include "util/problem.hpp"
+
+std::vector<std::pair<double, double>> toHexagon(
+    const std::vector<std::pair<double, double>>& _pos, double k) {
+  std::vector<std::pair<double, double>> pos = _pos;
+  int n = pos.size();
+  int sqrtN = static_cast<int>(std::sqrt(n));
+  double invSqrt3 = 1 / std::sqrt(3.0);
+
+  std::vector<int> args(n);
+  std::iota(args.begin(), args.end(), 0);
+  std::sort(args.begin(), args.end(),
+            [&pos](int a, int b) { return pos[a].first < pos[b].first; });
+  for (int i = 0; i < n; i++) pos[args[i]].first = i / sqrtN;
+
+  std::sort(args.begin(), args.end(), [&pos](int a, int b) { return pos[a] < pos[b]; });
+
+  for (int i = 0; i < n; ++i) {
+    pos[args[i]].second =
+        (i % sqrtN) * k + (static_cast<int>(pos[args[i]].first) % 2) * 0.5 * k;
+    pos[args[i]].first *= 1.5 * invSqrt3 * k;
+  }
+
+  return pos;
 }
 
-std::vector<std::pair<float, float>> hexagon(int n, float k) {
-  std::vector<std::pair<float, float>> ret = {{0, 0}};
-  if (n == 1) return ret;
+std::vector<std::vector<std::pair<double, double>>> solve(
+    const Problem& problem, std::vector<std::pair<double, double>>& pos) {
+  int n = problem.n;
+  double k = problem.k;
 
-  std::vector<std::tuple<int, int, int>> diffs = {{0, 1, -1}, {-1, 1, 0}, {-1, 0, 1},
-                                                  {0, -1, 1}, {1, -1, 0}, {1, 0, -1}};
+  pos = toHexagon(pos, k);
+  std::vector<std::vector<std::pair<double, double>>> positions = {pos};
 
-  int distFromCenter = 0;
-  while (true) {
-    int q = distFromCenter, r = -distFromCenter, s = 0;
-    for (int i = 0; i < 6; ++i) {
-      for (int j = 0; j < distFromCenter; ++j) {
-        ret.push_back(axialCoord2Euclidean(k * q, k * r));
-        if (ret.size() == n) return ret;
-        q += std::get<0>(diffs[i]);
-        r += std::get<1>(diffs[i]);
-        s += std::get<2>(diffs[i]);
-      }
+  std::vector<double> distance(n);
+
+  auto t0 = std::chrono::high_resolution_clock::now();
+
+  const int iterations = 1000;
+  for (int iter = 0; iter < iterations; ++iter) {
+    std::vector<std::pair<double, double>> displacement(n, {0.0, 0.0});
+
+    for (size_t i = 0; i < problem.m; i++) {
+      int u = problem.row[i];
+      int v = problem.col[i];
+      double delta_x = pos[u].first - pos[v].first;
+      double delta_y = pos[u].second - pos[v].second;
+      double dist = std::hypot(delta_x, delta_y);
+      double factor = -problem.data[i] * dist / k;
+      displacement[u].first += factor * delta_x;
+      displacement[u].second += factor * delta_y;
+      displacement[v].first -= factor * delta_x;
+      displacement[v].second -= factor * delta_y;
     }
-    distFromCenter += 1;
+
+    for (int i = 0; i < n; ++i) {
+      double norm = std::hypot(displacement[i].first, displacement[i].second);
+      if (norm > 0) {
+        displacement[i].first /= norm;
+        displacement[i].second /= norm;
+      }
+      displacement[i].first *= 2 * k;
+      displacement[i].second *= 2 * k;
+    }
+
+    for (int i = 0; i < n; ++i) {
+      pos[i].first += displacement[i].first;
+      pos[i].second += displacement[i].second;
+    }
+
+    pos = toHexagon(pos, k);
+    // dbg(problem.calcScore(pos, false));
+    positions.push_back(pos);
   }
+
+  auto t1 = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsed = t1 - t0;
+  std::cout << "Elapsed time: " << elapsed.count() << " seconds\n";
+
+  return positions;
 }
 
 int main() {
-  int n = 61;
-  float k = 1.0;
-  std::vector<std::pair<float, float>> hexagonList = hexagon(n, k);
+  Problem problem("494_bus");
 
-  for (const auto& coord : hexagonList) {
-    std::cout << "(" << coord.first << ", " << coord.second << ")\n";
+  std::vector<std::pair<double, double>> position(problem.n);
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<double> dist(0, 1);
+
+  for (int i = 0; i < int(problem.n); ++i) {
+    position[i] = {dist(gen), dist(gen)};
   }
+
+  std::vector<std::vector<std::pair<double, double>>> positions =
+      solve(problem, position);
+
+  problem.printOutput(positions);
 
   return 0;
 }
+
+// std::pair<double, double> axialCoord2Euclidean(int q, int r) {
+//   return {q + r / 2.0, r * std::sqrt(3) / 2.0};
+// }
+
+// std::vector<std::pair<double, double>> hexagon(int n, double k) {
+//   std::vector<std::pair<double, double>> ret = {{0, 0}};
+//   if (n == 1) return ret;
+
+//   std::vector<std::tuple<int, int, int>> diffs = {{0, 1, -1}, {-1, 1, 0}, {-1, 0, 1},
+//                                                   {0, -1, 1}, {1, -1, 0}, {1, 0,
+//                                                   -1}};
+
+//   int distFromCenter = 0;
+//   while (true) {
+//     int q = distFromCenter, r = -distFromCenter, s = 0;
+//     for (int i = 0; i < 6; ++i) {
+//       for (int j = 0; j < distFromCenter; ++j) {
+//         ret.push_back(axialCoord2Euclidean(k * q, k * r));
+//         if (int(ret.size()) == n) return ret;
+//         q += std::get<0>(diffs[i]);
+//         r += std::get<1>(diffs[i]);
+//         s += std::get<2>(diffs[i]);
+//       }
+//     }
+//     distFromCenter += 1;
+//   }
+// }
--- src/cpp/util/problem.hpp
+++ src/cpp/util/problem.hpp
@@ -1,30 +1,139 @@
 #pragma once
 
+#include <cassert>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
 #include <utility>
 #include <vector>
 
+#include "dbg.h"
+
 struct Problem {
-  size_t n;                  // number of vertices
-  size_t m;                  // number of edges
-  double k;                  // constant for score calculation
-  std::vector<size_t> row;   // edge u
-  std::vector<size_t> col;   // edge v
-  std::vector<double> data;  // edge weight
-  size_t t;                  // number of turns
+  size_t n;                         // number of vertices
+  size_t m;                         // number of edges
+  double k;                         // constant for score calculation
+  std::vector<size_t> row;          // edge u
+  std::vector<size_t> col;          // edge v
+  std::vector<double> data;         // edge weight
+  std::string matrixName = "test";  // matrix name
+
+  Problem(const std::string matrixName) : matrixName(matrixName) {
+    std::string curPath = std::filesystem::current_path().string();
+    assert(curPath.substr(curPath.size() - 8, 8) == "\\src\\cpp");
+    std::string path =
+        curPath.substr(0, curPath.size() - 8) + "\\data\\" + matrixName + ".mtx";
+
+    std::ifstream file(path);
+    if (!file.is_open()) {
+      std::cerr << "Error: file not found\n";
+      exit(1);
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+      if (line[0] == '%') continue;
+      break;
+    }
+
+    size_t _n, _m;
+    std::istringstream iss(line);
+    iss >> n >> _n >> _m;
+    assert(n == _n);
+
+    std::map<std::pair<size_t, size_t>, double> edges;
+    for (size_t i = 0; i < _m; ++i) {
+      std::getline(file, line);
+      std::istringstream iss(line);
+      int r, c;
+      double w;
+      iss >> r >> c >> w;
+      r--;
+      c--;
+      if (r == c) continue;
+      edges[std::minmax(r, c)] += std::abs(w);
+    }
+
+    m = edges.size();
+    row.reserve(m);
+    col.reserve(m);
+    data.reserve(m);
+
+    for (const auto& [rc, w] : edges) {
+      row.push_back(rc.first);
+      col.push_back(rc.second);
+      data.push_back(w);
+    }
+
+    file.close();
+    k = 1 / std::sqrt(n);
+  }
 
   Problem(size_t n, size_t m, double k, std::vector<size_t> row,
-          std::vector<size_t> col, std::vector<double> data, size_t t)
-      : n(n), m(m), k(k), row(row), col(col), data(data), t(t) {}
+          std::vector<size_t> col, std::vector<double> data)
+      : n(n), m(m), k(k), row(row), col(col), data(data) {}
+
+  double calcScore(const std::vector<std::pair<double, double>>& pos,
+                   bool addRepulsive = true) const {
+    double score = 0.0;
+
+    if (addRepulsive) {
+      for (size_t i = 0; i < n; ++i) {
+        for (size_t j = i + 1; j < n; ++j) {
+          double x1 = pos[i].first;
+          double y1 = pos[i].second;
+          double x2 = pos[j].first;
+          double y2 = pos[j].second;
+          double d = std::hypot(x1 - x2, y1 - y2);
+          if (d < 1e-9) {
+            dbg(i, j, x1, y1, x2, y2, d);
+            assert(false);
+          }
+          score -= std::pow(k, 2) * std::log(d);
+        }
+      }
+    }
+
+    for (size_t i = 0; i < m; ++i) {
+      size_t u = row[i];
+      size_t v = col[i];
+      double a = data[i];
+      double x1 = pos[u].first;
+      double y1 = pos[u].second;
+      double x2 = pos[v].first;
+      double y2 = pos[v].second;
+      double d = std::hypot(x1 - x2, y1 - y2);
+      score += a * std::pow(d, 3) / (3.0 * k);
+    }
+
+    return score;
+  }
 
   // positions[turn][vertex]
   void printOutput(std::vector<std::vector<std::pair<double, double>>> positions) {
-    std::cout << n << " " << m << " " << k << "\n";
-    for (size_t i = 0; i < m; ++i) {
-      std::cout << row[i] << " " << col[i] << " " << data[i] << "\n";
+    std::string curPath = std::filesystem::current_path().string();
+    assert(curPath.substr(curPath.size() - 8, 8) == "\\src\\cpp");
+    std::string path =
+        curPath.substr(0, curPath.size() - 8) + "\\out\\" + matrixName + ".out";
+    std::ofstream file(path);
+    if (!file.is_open()) {
+      dbg(path);
+      std::cerr << "Error: file not found\n";
+      exit(1);
     }
-    std::cout << t << "\n";
+
+    file << n << " " << m << " " << k << "\n";
+    for (size_t i = 0; i < m; ++i) {
+      file << row[i] << " " << col[i] << " " << data[i] << "\n";
+    }
+    file << positions.size() << "\n";
     for (const auto& turn : positions)
-      for (const auto& pos : turn) std::cout << pos.first << " " << pos.second << "\n";
+      for (const auto& pos : turn) file << pos.first << " " << pos.second << "\n";
+    file.close();
+
+    std::cout << "Output path:" << path << "\n";
   }
 };
