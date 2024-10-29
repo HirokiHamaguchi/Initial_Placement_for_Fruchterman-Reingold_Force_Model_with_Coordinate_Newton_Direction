7b0095a962624ab9f40c5eb0cfced6e31c724b2c
hari64 <hari64boli64@gmail.com>
2024-10-29 11:34:54 +0900

    add plot

--- doc/main/individual/individual.cpp
+++ doc/main/individual/individual.cpp
@@ -5,7 +5,7 @@ int main() {
       "cycle300", "jagmesh1",  // my selection
       "btree9",                // from networkx
       "1138_bus", "dwt_1005", "dwt_2680",
-      // "3elt", "USpowerGrid",  // SGD
+      "3elt",  // SGD
   };
 
   std::vector<Method> methods = {FR, RS_FR, L_BFGS, RS_L_BFGS};
@@ -13,7 +13,7 @@ int main() {
   std::string histStr =
       std::to_string(matrixNames.size()) + " " + std::to_string(methods.size()) + "\n";
 
-  const int MAX_ITER = 300;
+  const int MAX_ITER = 200;
 
   for (std::string matrixName : matrixNames) {
     Problem problem(matrixName);
@@ -23,7 +23,7 @@ int main() {
       histStr += MethodStr[method] + "\n";
       std::cout << MethodStr[method] << std::endl;
       // Solve by each method
-      for (int seed = 0; seed < 10; seed++) {
+      for (int seed = 0; seed < 3; seed++) {
         auto [hist, positions] = solve(method, problem, true, seed, MAX_ITER);
         // Output
         histStr += std::to_string(hist.size()) + "\n";
@@ -37,8 +37,8 @@ int main() {
         std::cout << "Elapsed time: " << time << " seconds" << std::endl;
         std::cout << "Score: " << score << std::endl;
         if (seed == 0)
-          problem.printOutput(positions,
-                              problem.matrixName + "_" + MethodStr[method] + ".out");
+          problem.printOutput(positions, "out/" + problem.matrixName + "_" +
+                                             MethodStr[method] + ".out");
       }
     }
   }
