f9fe6430565fd6e2dfa3a6e5a1fe82a42b750543
Hiroki Hamaguchi <hari64boli64@gmail.com>
2026-06-10 12:46:45 +0900

    add data

--- data/_ssget.py
+++ data/_ssget.py
@@ -103,28 +103,86 @@ def main2():
     for n in [50, 100, 200, 300, 500]:
         G = nx.circulant_graph(n, [1])
         nx2mtx(G, f"cycle{n}.mtx")
-    for a, b, c in [(3, 2, 6)]:
+
+
+def relabel_int(G: nx.Graph) -> nx.Graph:
+    return nx.convert_node_labels_to_integers(G, ordering="sorted")
+
+
+def make_cylinder_grid(m: int, n: int) -> nx.Graph:
+    """
+    m x n cylindrical grid.
+    Periodic in the second dimension, open in the first dimension.
+
+    Nodes are initially (i, j), then relabeled to 0, ..., mn-1.
+    """
+    G = nx.Graph()
+
+    for i in range(m):
+        for j in range(n):
+            G.add_node((i, j))
+
+            # vertical edge
+            if i + 1 < m:
+                G.add_edge((i, j), (i + 1, j))
+
+            # periodic horizontal edge
+            G.add_edge((i, j), (i, (j + 1) % n))
+
+    return relabel_int(G)
+
+
+def _sierpinski_recursive(order: int):
+    """
+    Returns a Sierpinski gasket graph and its three outer corner nodes.
+    """
+    if order == 0:
         G = nx.Graph()
-        for i in range(a):
-            G.add_edge(i, (i + 1) % a)
-        for i in range(a):
-            for j in range(b):
-                idx = a + c * (b * i + j)
-                G.add_edge(i, idx)
-                for k in range(c):
-                    G.add_edge(idx + k, idx + (k + 1) % c)
-        nx2mtx(G, f"_circleHandmade_{a}_{b}_{c}.mtx")
+        G.add_edges_from([(0, 1), (1, 2), (2, 0)])
+        return G, (0, 1, 2)  # top, left, right
+
+    G0, c0 = _sierpinski_recursive(order - 1)
+    n = len(G0)
+
+    H = nx.disjoint_union_all([G0, G0.copy(), G0.copy()])
+
+    A = (c0[0], c0[1], c0[2])
+    B = tuple(x + n for x in c0)
+    C = tuple(x + 2 * n for x in c0)
+
+    # Identify adjacent corners of the three smaller triangles.
+    H = nx.contracted_nodes(H, A[1], B[0], self_loops=False)
+    H = nx.contracted_nodes(H, A[2], C[0], self_loops=False)
+    H = nx.contracted_nodes(H, B[2], C[1], self_loops=False)
+
+    outer_corners = (A[0], B[1], C[2])
+
+    mapping = {old: i for i, old in enumerate(H.nodes())}
+    H = nx.relabel_nodes(H, mapping, copy=True)
+
+    return H, tuple(mapping[v] for v in outer_corners)
+
+
+def make_sierpinski(order: int) -> nx.Graph:
+    G, _ = _sierpinski_recursive(order)
+    return relabel_int(G)
+
+
+def make_grid(m: int, n: int) -> nx.Graph:
+    """
+    Ordinary m x n grid graph.
+    Added as a simple regular-graph baseline in response to the reviewer.
+    """
+    G = nx.grid_2d_graph(m, n)
+    return relabel_int(G)
 
 
 def main3():
-    # additional large graphs
+    # additional graphs from SuiteSparse / Florida collection
     manual_names = [
         "jagmesh8",
-        "bcsstk14",
-        "bcsstk15",
-        "bcsstk16",
+        "crack",
         "USpowerGrid",
-        "bcspwr10",
         "wiki-Vote",
     ]
 
@@ -132,6 +190,16 @@ def main3():
         mat = ssgetpy.search(name)[0]
         result = process_matrix(mat, force=True)
         assert result is not None, f"{name} failed processing"
+
+    generated_graphs = {
+        "cylinder_32_32.mtx": make_cylinder_grid(32, 32),
+        "sierpinski_06.mtx": make_sierpinski(6),
+        "grid_32_32.mtx": make_grid(32, 32),
+    }
+
+    for filename, G in generated_graphs.items():
+        assert nx.is_connected(G), f"{filename} is not connected"
+        nx2mtx(G, filename)
 
 
 if __name__ == "__main__":
